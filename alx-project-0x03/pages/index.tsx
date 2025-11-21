925dd2a23ad3b5e586a98f5dee6b6e540e0b1408
Ibrahim Ayinde <mi.wordpressdesign@gmail.com>
2025-11-21 09:55:02 +0100

    Implementing a share layout

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,13 @@
 import Layout from "@/components/layouts/Layout";
 
 function Home() {
-  return <Layout children={undefined} />;
+  return (
+    <>
+      <Layout children={undefined} />
+
+      <h1>Welcome to Splash App</h1>
+    </>
+  );
 }
 
 export default Home;
